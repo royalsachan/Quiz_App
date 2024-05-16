6c64a4424ab049b2aeb75427f04c1f0bf04c8ce2
Royal <royalsachaniiitm@gmail.com>
2024-05-16 16:30:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ function selectAnswer(e){
   const isCorrect = selectedBtn.dataset.correct === "true" ;
    if(isCorrect) {
     selectedBtn.classList.add("correct");
-    //score++;
+    score++;
   }
   else{
     selectedBtn.classList.add("incorrect");
